cc5b27368841655952546da6d57acf33c414f38a
BarbudaontheBoat <joao.patricio999@gmail.com>
2015-02-20 14:34:38 +0000

    My Programming Assignment 2 solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function creates a list containing the functions:
+##          set <- set the value of the matrix
+##          get <- get the value of the matrix
+##          setinverse <- set the value of the matrix inverse
+##          getinverse <- get the value of the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    m <- NULL
+    
+    set <- function(y) {
+        
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## This function calculates the inverse of the matrix x
+## If x is a singular matrix, a warning message will apear
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    if(det(x$get()) == 0) {
+        
+        message("The input matrix is singular, has no inverse")
+    }
+    if(det(x$get()) != 0) {
+        
+        m <- x$getinverse()
+        
+        if(!is.null(m)) {
+            
+            message("getting cached data")
+            return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+    }
 }
